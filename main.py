368ddbfbc43921a1cb364b9ce8c1ff55060e68da
khawarizmiharis <161565929+khawarizmiharis@users.noreply.github.com>
2024-02-28 15:12:33 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+name = "khawarizmi"
+age = "18"
+live = "sukabumi"
+hobby = "badminton"
+gender = "male"
+
+print("my name is =" ,name)
+print("my age is =" ,age )
+print("i live in =" ,live)
+print("my hobby =" ,hobby)
+print("my gender =" ,gender)
